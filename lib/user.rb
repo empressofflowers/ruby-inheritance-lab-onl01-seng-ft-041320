63ebc869a34152efff785529a56e77c546cec29e
Devone Austin <devone.austin@gmail.com>
2020-04-29 19:35:33 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -7,12 +7,4 @@ class User
     @first_name = first_name
     @last_name = last_name
   end
-
-  def first_name name
-    User.first_name(name)
-  end
-
-  def last_name name
-    User.last_name(name)
-  end
 end
